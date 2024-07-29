91a56d92ffa0f2ac918d80a1e18f7904de0c5bde
Daniel Pichardo <danielpichardo716@gmail.com>
2024-07-28 23:54:25 -0400

    fix imports and type error

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -120,7 +120,7 @@ export default function CodePage() {
           <div className="flex flex-col-reverse gap-y-4">
             {messages.map((message) => (
               <div
-                key={message.content}
+                key={`${message.content}`}
                 className={cn(
                   "p-8 w-full flex items-start gap-x-8 rounded-lg",
                   message.role === "user"
@@ -142,7 +142,7 @@ export default function CodePage() {
                   }}
                   className="text-sm overflow-hidden leading-7"
                 >
-                  {message.content || ""}
+                  {`${message.content}` || ""}
                 </ReactMarkdown>
               </div>
             ))}
